3f3b84c5ee5e5fa1557084fd2d39af09cffbaf1d
thxinsomnia <kompenizeko@gmail.com>
2024-10-14 12:32:34 +0700

    Customize Swiper, Fix Image, and Minor Error

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
+    // Optional parameters=
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    autoplay: {
+        delay: 3000},
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
   });
