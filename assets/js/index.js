8c7143b01b22508eee313ac0ddf406fd0e4eaaa3
Dany <muhammaddeny000@gmail.com>
2024-10-15 23:38:44 +0700

    product improvement

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,35 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    const popups = document.querySelectorAll('.order-popup');
+    const closePopupButtons = document.querySelectorAll('.close-popup');
+  
+    // Loop untuk setiap tombol "Add to Cart"
+    addToCartButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        // Menampilkan popup sesuai dengan indeks tombol yang diklik
+        popups[index].style.display = 'flex';
+      });
+    });
+  
+    // Loop untuk setiap tombol "Batal"
+    closePopupButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        // Menyembunyikan popup sesuai dengan indeks tombol yang diklik
+        popups[index].style.display = 'none';
+      });
+    });
+  
+    // Menutup popup jika area luar popup diklik
+    window.addEventListener('click', (event) => {
+      popups.forEach((popup) => {
+        if (event.target === popup) {
+          popup.style.display = 'none';
+        }
+      });
+    });
+  });
+  
