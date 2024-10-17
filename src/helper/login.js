05bcde25853eca3ddf1ce20e61a9117b178acc5a
Zamy03 <ukasyzzam@gmail.com>
2024-10-18 02:23:54 +0700

    try and error 2 login with local server

--- src/helper/login.js
+++ src/helper/login.js
@@ -1,11 +1,12 @@
-// import { endpointLogin } from "/helper/url.js";
+import { endpointLogin } from "./../helper/url.js";
 // console.log(endpointLogin);
-
 
 document.querySelector(".login-form").addEventListener("submit", async function (event) {
     event.preventDefault();
     const formData = new FormData(this);
     const data = Object.fromEntries(formData);
+    
+    // Melakukan permintaan POST ke endpoint login tanpa menggunakan token
     const response = await fetch("http://localhost:8080/login", {
         method: "POST",
         headers: {
@@ -13,22 +14,55 @@ document.querySelector(".login-form").addEventListener("submit", async function 
         },
         body: JSON.stringify(data),
     });
+    
     const responseData = await response.json();
+    
     if (response.ok) {
-        localStorage.setItem("token", responseData.token);
+        // Jika login berhasil, langsung pindah ke halaman index.html tanpa menyimpan token
         window.location.replace("/index.html");
-        alert("percobaan berhasil");
+        alert("Login berhasil");
         console.log(responseData);
         
     } else {
+        // Tampilkan pesan error jika login gagal
         alert(responseData.message);
     }
 });
 
 function getUserDetails() {
-    return fetch(endpointLogin, {
-        headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-    }).then((response) => response.json());
+    // Melakukan fetch tanpa menggunakan header Authorization
+    return fetch(endpointLogin)
+        .then((response) => response.json());
 }
+
+
+// document.querySelector(".login-form").addEventListener("submit", async function (event) {
+//     event.preventDefault();
+//     const formData = new FormData(this);
+//     const data = Object.fromEntries(formData);
+//     const response = await fetch("http://localhost:8080/login", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(data),
+//     });
+//     const responseData = await response.json();
+//     if (response.ok) {
+//         localStorage.setItem("token", responseData.token);
+//         window.location.replace("/index.html");
+//         alert("percobaan berhasil");
+//         console.log(responseData);
+        
+//     } else {
+//         alert(responseData.message);
+//     }
+// });
+
+// function getUserDetails() {
+//     return fetch(endpointLogin, {
+//         headers: {
+//             Authorization: `Bearer ${localStorage.getItem("token")}`,
+//         },
+//     }).then((response) => response.json());
+// }
