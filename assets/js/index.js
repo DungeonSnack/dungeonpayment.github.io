9e1b54edec3640685cf086dc13c703b6b000647a
Dany <muhammaddeny000@gmail.com>
2024-10-20 10:05:14 +0700

    fix tonggel issue

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,36 +12,6 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addToCartButtons = document.querySelectorAll(".add-to-cart");
-  const popups = document.querySelectorAll(".order-popup");
-  const closePopupButtons = document.querySelectorAll(".close-popup");
-
-  // Loop untuk setiap tombol "Add to Cart"
-  addToCartButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      // Menampilkan popup sesuai dengan indeks tombol yang diklik
-      popups[index].style.display = "flex";
-    });
-  });
-
-  // Loop untuk setiap tombol "Batal"
-  closePopupButtons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      // Menyembunyikan popup sesuai dengan indeks tombol yang diklik
-      popups[index].style.display = "none";
-    });
-  });
-
-  // Menutup popup jika area luar popup diklik
-  window.addEventListener("click", (event) => {
-    popups.forEach((popup) => {
-      if (event.target === popup) {
-        popup.style.display = "none";
-      }
-    });
-  });
-});
 
 document.addEventListener("DOMContentLoaded", function () {
   // Scroll ke posisi yang diinginkan (misalnya 100px dari atas)
