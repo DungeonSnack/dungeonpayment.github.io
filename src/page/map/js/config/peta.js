acffa29658a683b122edf26422bd9965b1c456bc
thxinsomnia <kompenizeko@gmail.com>
2024-10-23 21:50:46 +0700

    Try Add Marker

--- src/page/map/js/config/peta.js
+++ src/page/map/js/config/peta.js
@@ -6,6 +6,12 @@ import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
 import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
 import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
 import {container} from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js';
+import {Vector as VectorLayer} from 'https://cdn.skypack.dev/ol/layer.js';
+import Feature from 'https://cdn.skypack.dev/ol/Feature.js';
+import Point from 'https://cdn.skypack.dev/ol/geom/Point.js';
+import Style from 'https://cdn.skypack.dev/ol/style/Style.js';
+import Icon from 'https://cdn.skypack.dev/ol/style/Icon.js';
+
 
 const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; Powered by PetaPedia Indonesia</a> ';
 
@@ -49,3 +55,34 @@ export let map = new Map({
   view: defaultstartmap,
 });
 
+
+// Buat fitur marker
+const markerFeature = places.map(place => {
+const markerFeature = new Feature({
+  geometry: new Point(fromLonLat(place)), // Ganti dengan koordinat marker
+})}) ;
+
+// Atur gaya untuk marker
+markerFeature.setStyle(new Style({
+  image: new Icon({
+    src: 'https://openlayers.org/en/latest/examples/data/icon.png', // URL ikon
+    scale: 0.1, // Ubah skala sesuai kebutuhan
+  }),
+}));
+
+// Buat sumber dan layer untuk marker
+const vectorSource = new VectorSource({
+  features: [markerFeature],
+});
+const markerLayer = new VectorLayer({
+  source: vectorSource,
+});
+
+// Tambahkan layer marker ke peta
+map.addLayer(markerLayer);
+
+const places = [
+  [107.57567676530189, -6.874328155615692], // Marker 1
+  [107.57717560948039, -6.875409662859955], // Marker 2
+  [107.582000, -6.877000], // Marker 3
+];
