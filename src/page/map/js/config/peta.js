11fd1d7f1d8e78d47484f32f961abd122060260b
thxinsomnia <kompenizeko@gmail.com>
2024-10-25 00:08:01 +0700

    Sorting, Cleaning, and Tidying Code pt3

--- src/page/map/js/config/peta.js
+++ src/page/map/js/config/peta.js
@@ -1,21 +1,21 @@
-import Map from 'https://cdn.skypack.dev/ol/Map.js';
-import View from 'https://cdn.skypack.dev/ol/View.js';
-import TileLayer from 'https://cdn.skypack.dev/ol/layer/Tile.js';
-import OSM from 'https://cdn.skypack.dev/ol/source/OSM.js';
-import {fromLonLat} from 'https://cdn.skypack.dev/ol/proj.js';
-import Overlay from 'https://cdn.skypack.dev/ol/Overlay.js';
-import {container} from 'https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js';
-import { getUserLocation } from '../controller/geolocation.js';
+import Map from "https://cdn.skypack.dev/ol/Map.js";
+import View from "https://cdn.skypack.dev/ol/View.js";
+import TileLayer from "https://cdn.skypack.dev/ol/layer/Tile.js";
+import OSM from "https://cdn.skypack.dev/ol/source/OSM.js";
+import { fromLonLat } from "https://cdn.skypack.dev/ol/proj.js";
+import Overlay from "https://cdn.skypack.dev/ol/Overlay.js";
+import { container } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
+import { getUserLocation } from "../controller/geolocation.js";
 
+const attributions =
+  '<a href="https://petapedia.github.io/" target="_blank">&copy; Powered by PetaPedia Indonesia</a> ';
 
-const attributions = '<a href="https://petapedia.github.io/" target="_blank">&copy; Powered by PetaPedia Indonesia</a> ';
+const place = [107.578624, -6.876631];
 
-const place = [	107.578624,-6.876631];
-
-export let idmarker = {id:1};
+export let idmarker = { id: 1 };
 
 const basemap = new TileLayer({
-  source: new OSM({attributions: attributions,}),
+  source: new OSM({ attributions: attributions }),
 });
 
 const defaultstartmap = new View({
@@ -24,29 +24,27 @@ const defaultstartmap = new View({
 });
 
 export const overlay = new Overlay({
-    element: container('popup'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
+  element: container("popup"),
+  autoPan: {
+    animation: {
+      duration: 250,
     },
-  });
+  },
+});
 
 export const popupinfo = new Overlay({
-    element: container('popupinfo'),
-    autoPan: {
-      animation: {
-        duration: 250,
-      },
+  element: container("popupinfo"),
+  autoPan: {
+    animation: {
+      duration: 250,
     },
+  },
 });
 
 export let map = new Map({
-  overlays: [overlay,popupinfo],
-  target: 'map',
-  layers: [
-    basemap
-  ],
+  overlays: [overlay, popupinfo],
+  target: "map",
+  layers: [basemap],
   view: defaultstartmap,
 });
- getUserLocation(map)
+getUserLocation(map);
