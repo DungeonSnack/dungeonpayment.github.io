9909af6cd0d23fcf93391d764da354d6468b6bdc
thxinsomnia <kompenizeko@gmail.com>
2024-10-28 20:51:38 +0700

    Swiver Customization

--- src/payment/payment.js
+++ src/payment/payment.js
@@ -107,3 +107,16 @@ document.querySelectorAll('.faq-question').forEach((item) => {
     });
 });
 
+const swiper = new Swiper(".swiper", {
+    // Optional parameters=
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
