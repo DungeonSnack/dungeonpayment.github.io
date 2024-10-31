59106d7c4c26f9efd95d8e6c07ee16bbf49af65c
thxinsomnia <kompenizeko@gmail.com>
2024-10-31 22:59:04 +0700

    TRY add login with qr pt2

---
+++ src/page/loginscan/loginscan.js
@@ -0,0 +1,22 @@
+import {
+  qrController,
+  deleteCookie,
+} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/whatsauth.js";
+import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
+import loadComponent from "/src/helpers/loadComponent.js";
+
+const promise = loadComponent("", "/src/pages/login-qr/login-qr.html");
+
+promise
+  .then(() => {
+    wauthparam.auth_ws = "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
+    //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
+    wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82Mjg4MTAyMjUyMjkyMA==";
+    wauthparam.tokencookiehourslifetime = 18;
+    wauthparam.redirect = "/";
+    deleteCookie(wauthparam.tokencookiename);
+    qrController(wauthparam);
+  })
+  .catch((error) => {
+    console.error("Error loading page:", error);
+  });
